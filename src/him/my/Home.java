63e084a5b2730b229e56171f1ba52929b4b66453
Himanshu <Himvais@hotmail.com>
2013-01-20 16:41:16 +0530

    Loading android Project
    Loaded the my android project here, with all files neccesory.

---
+++ src/him/my/Home.java
@@ -0,0 +1,12 @@
+package him.my;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Home extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
